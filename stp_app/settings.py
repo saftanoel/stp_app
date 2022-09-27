1f9e9528875b159919f781a38ea97f727db83802
saftanoel <george.safta@albascout.ro>
2022-09-27 20:39:35 +0300

    27 septembrie

--- stp_app/settings.py
+++ stp_app/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'django_cron'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,14 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRON_CLASSES = [
+    "commands.get_bus.MyCronJob",
+    # ...
+]
+
+REST_FRAMEWORK = {
+  'DEFAULT_PERMISSION_CLASSES': [
+      'rest_framework.permissions.AllowAny',
+  ],
+}
